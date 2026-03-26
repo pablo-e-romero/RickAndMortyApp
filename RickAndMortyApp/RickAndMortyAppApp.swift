cd7c3a64b1d01766e5b3888d5ec1c46eacb56038
Pablo Romero <romero7116@gmail.com>
2026-03-26 23:01:33 +0100

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Pablo Romero on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppTests.swift
+//  RickAndMortyAppTests
+//
+//  Created by Pablo Romero on 26/3/26.
+//
+
+import Testing
+@testable import RickAndMortyApp
+
+struct RickAndMortyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
